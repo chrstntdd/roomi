90a15215611653ffee5de73e9c72eb7671f9874f
Christian Todd <christiantodd27@icloud.com>
2018-08-20 21:31:23 -0400

    add classnames util.

--- src/client/util/index.ts
+++ src/client/util/index.ts
@@ -1,1 +1,29 @@
 export const noop = () => {};
+
+/**
+ * @description
+ * To conditionally join classnames
+ *
+ * @param arr An array of strings or expressions that evaluate to a string
+ */
+export const classNames = (arr: string[]): string => {
+  let classes = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const arg = arr[i];
+    if (!arg) continue;
+
+    if (typeof arg === 'string' || typeof arg === 'number') {
+      classes.push(arg);
+    } else if (Array.isArray(arg) && arg.length) {
+      const inner = classNames.apply(null, arg);
+      if (inner) classes.push(inner);
+    }
+  }
+
+  return classes.join(' ');
+};
+
+/* STRING FORMATTING */
+export const capitalizeFirstChar = (value: string): string =>
+  value.charAt(0).toUpperCase() + value.slice(1);
